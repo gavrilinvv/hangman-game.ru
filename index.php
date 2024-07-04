cb0530cdd5ef99dc4920f251cf4fd08119d9cf1a
Slava <gavrilin2410@gmail.com>
2024-07-04 21:58:59 +0300

    add ya metrika

--- index.php
+++ index.php
@@ -16,7 +16,6 @@
 	<meta name="description" content="Виселица это игра, в которой надо разгадать загаданное слово за несколько попыток. Онлайн, бесплатно и без регистрации" />
 	<meta property="og:description" content="Виселица это игра, в которой надо разгадать загаданное слово за несколько попыток. Онлайн, бесплатно и без регистрации" />
 	<meta name="keywords" content="Виселица, бесплатная браузерная игра, разгадать слово, играть без регистрации, онлайн" />
-	<meta name="yandex-verification" content="738fcd3e75cf5f96" />
 
 	<!-- <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
 	<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
@@ -25,6 +24,24 @@
 	<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
 	<meta name="msapplication-TileColor" content="#da532c">
 	<meta name="theme-color" content="#ffffff">-->
+
+	<!-- Yandex.Metrika counter -->
+	<script type="text/javascript" >
+	(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+	m[i].l=1*new Date();
+	for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+	k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+	(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+	ym(97750844, "init", {
+			clickmap:true,
+			trackLinks:true,
+			accurateTrackBounce:true,
+			webvisor:true
+	});
+	</script>
+	<noscript><div><img src="https://mc.yandex.ru/watch/97750844" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+	<!-- /Yandex.Metrika counter -->
 </head>
 <body>
 	<div id="app">
